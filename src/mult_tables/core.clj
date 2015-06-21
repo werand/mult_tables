718c807d4e03f19053e3c84e9de421fbaeb6ad31
Andreas Werner <werner_andreas@web.de>
2015-06-21 23:14:43 +0200

    Added information text

--- src/mult_tables/core.clj
+++ src/mult_tables/core.clj
@@ -5,13 +5,15 @@
 ;; The idea for this visualization comes from this video
 ;; https://www.youtube.com/watch?v=-X49VQgi86E&feature=youtu.be
 
-(def window-size [620 620])
+(def window-size [620 630])
 
 (def circle-diameter {:x 580 :y 580})
 
+(def offset 20)
+
 (def initial-state {:modulo 16
                     :multiplicator 7
-                    :next-modulo-fn inc})
+                    :next-modulo-fn identity})
 
 (defn change-in-state [key f state]
   (assoc state key (f (key state))))
@@ -33,9 +35,6 @@
   ;; The initial state
   initial-state)
 
-(defn set-state! [multiplicator modulo]
-  (reset! state {:modulo modulo :multiplicator multiplicator}))
-
 (defn update-state [state]
   (assoc state :modulo ((:next-modulo-fn state) (:modulo state))))
 
@@ -56,17 +55,23 @@
 (defn draw-line [modulo v1 v2]
   (q/line (calc-position modulo v1) (calc-position modulo v2)))
 
+(defn draw-text [modulo multiplicator]
+  (q/fill 0)
+  (q/text (str "Mod:  " modulo) 20 600)
+  (q/text (str "Mult: " multiplicator) 20 620))
+
 (defn draw-state [state]
   ;; Clear the sketch by filling it with light-grey color.
   (q/background 240)
   (q/fill 255)
-  (q/with-translation [(/ (q/width) 2)
-                       (/ (q/height) 2)]
-    (q/ellipse 0 0 (:x circle-diameter) (:y circle-diameter))
-    (let [{:keys [:modulo :multiplicator]} state]
+  (let [{:keys [:modulo :multiplicator]} state]
+    (q/with-translation [(+ offset (/ (:x circle-diameter) 2))
+                         (+ offset (/ (:y circle-diameter) 2))]
+      (q/ellipse 0 0 (:x circle-diameter) (:y circle-diameter))
       (doseq [i (range modulo)]
         (draw-modulo-pos modulo i)
-        (draw-line modulo i (* i multiplicator))))))
+        (draw-line modulo i (* i multiplicator))))
+    (draw-text modulo multiplicator)))
 
 (q/defsketch mult-tables
   :title "Multiplication magic"
